1a0443880576406532d5babb3921f76cbe8d3892
tuanbaole <letuanbao1993@gmail.com>
2017-03-25 13:53:46 +0700

    config

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,8 +32,6 @@ App::uses('Controller', 'Controller');
  */
 class AppController extends Controller {
 	public $components = array('DebugKit.Toolbar','Session');
-	
-	var $layout = 'layout1';
 
 	public $helpers = array(
         		'Html'=> array('className'=>'MyHtml')
--- app/webroot/js/ketqua/index.js
+++ app/webroot/js/ketqua/index.js
@@ -1,17 +1,19 @@
 jQuery(document).ready(function(){
+
+	var date = jQuery('#get-date').attr('date');
+	var ketqua_id = jQuery('#ket-qua-id').attr('don-vi');
+
 	jQuery('#diem').blur(function(){
 		var loto = jQuery('#loto').val();
 		var diem = jQuery(this).val();
 		if (loto !== '' && diem !== '') 
 		{
 			if (loto >= 0 && loto <= 100) {
-				var date = jQuery('#get-date').attr('date');
 				var gia_diem = jQuery('#gia-diem').attr('don-vi');
 				var gia_trung = jQuery('#gia-trung').attr('don-vi');
-				var ketqua_id = jQuery('#ket-qua-id').attr('don-vi');
 				var data = {loto:loto,diem:diem,date:date,gia_diem:gia_diem,gia_trung:gia_trung,ketqua_id:ketqua_id};
 				jQuery.ajax({
-	                url: "/ketquas/nhapvao",
+	                url: "/ketquas/nhapvao_lo",
 	                type: "POST",
 	                data: data,
 	                beforeSend: function () {
@@ -19,6 +21,8 @@ jQuery(document).ready(function(){
 	                success: function (result) {
 	                	jQuery('#loto').val('');
 						jQuery('#diem').val('');
+						jQuery('#giaithuong-loto').html(result);
+    					boostrapAlerSuccess('Thêm mới thành công',5000);
 	                },
 	                error: function() {
 	                	boostrapAlerDanger('Chưa có kết quả tính toán',5000);
@@ -41,50 +45,52 @@ jQuery(document).ready(function(){
 
 	});
 
-	function boostrapAlerDanger(content = '',time = 5000) 
-	{
-		var rString = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-		var html = '<div class="alert alert-danger alert-dismissable" id="'+rString+'"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+content+'</div>';
-		jQuery('#alert-notification').fadeIn().append(html);
-		setTimeout(function(){ 
-			jQuery('#alert-notification').find('#'+rString).fadeOut().remove();
-		}, time);
-	};
-
-	function boostrapAlerSuccess(content = '',time = 5000) 
-	{
-		var rString = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-		var html = '<div class="alert alert-success alert-dismissable" id="'+rString+'"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'+content+'</div>';
-		jQuery('#alert-notification').fadeIn().append(html);
-		setTimeout(function(){ 
-			jQuery('#alert-notification').find('#'+rString).fadeOut().remove();
-		}, time);
-	};
-
-	function randomString(length, chars) {
-	    var result = '';
-	    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-	    return result;
-	}
-
-	jQuery('.remove-giai-thuong').click(function(){
-		if (confirm('Bạn có chắc xóa dòng này đi không?')) {
-			var giaithuong_id = jQuery(this).attr('gt-id');
-			var data = {giaithuong_id:giaithuong_id};
-			jQuery.ajax({
-                url: "/ketquas/delete_giaithuong",
-                type: "POST",
-                data: data,
-                beforeSend: function () {
-                },
-                success: function (result) {
-					boostrapAlerSuccess('Xóa thành công',5000);
-                },
-                error: function() {
-                	boostrapAlerDanger('Xóa không thành công',5000);
-                }
-            });	
+	jQuery('#tien_de').blur(function(){
+		var de = jQuery('#de').val();
+		var tien_de = jQuery(this).val();
+		if (de !== '' && tien_de !== '') 
+		{
+			if (de >= 0 && de <= 100) {
+				var gia_tien_de = jQuery('#gia-tien_de').attr('don-vi');
+				var gia_trung = jQuery('#gia-trung').attr('don-vi');
+				var data = {de:de,tien_de:tien_de,date:date,gia_tien_de:gia_tien_de,gia_trung:gia_trung,ketqua_id:ketqua_id};
+				jQuery.ajax({
+	                url: "/ketquas/nhapvao_lo",
+	                type: "POST",
+	                data: data,
+	                beforeSend: function () {
+	                },	
+	                success: function (result) {
+	                	jQuery('#de').val('');
+						jQuery('#tien_de').val('');
+						jQuery('#giaithuong-de').html(result);
+    					boostrapAlerSuccess('Thêm mới thành công',5000);
+	                },
+	                error: function() {
+	                	boostrapAlerDanger('Chưa có kết quả tính toán',5000);
+	                }
+	            });	
+			} 
+			else 
+			{
+				jQuery('#de').val('');
+				jQuery(this).val('');
+				boostrapAlerDanger('Hãy nhập giá trị <strong>Đề</strong> từ 00 - 99',5000);
+			}
+		} 
+		else 
+		{
+			jQuery('#de').val('');
+			jQuery(this).val('');
+			boostrapAlerDanger('Hãy nhập giá trị vào các <strong>Đề</strong> và <strong>Tiền</strong>',5000);
 		}
+
 	});
+
+	jQuery('#datepicker').datepicker({
+		format: "yyyy-mm-dd"
+	});
+	jQuery('.icon-arrow-left').addClass('glyphicon glyphicon-arrow-left');
+	jQuery('.icon-arrow-right').addClass('glyphicon glyphicon-arrow-right');
 
 });
